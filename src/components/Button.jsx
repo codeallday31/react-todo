e5e30cf58b7af41aeec52ac944d6c02af984f553
Jhark VIllegas <jhark_@Jharks-MacBook-Air.local>
2023-03-22 01:40:11 +0800

    add components button

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Button({ children, ...props }) {
+    const { onClick, ...rest } = props
+    return (
+        <>
+            <button
+                onClick={onClick}
+                className='px-3 flex items-center border p-1 bg-indigo-300 rounded-md'
+            >
+                {children}
+            </button>
+        </>
+    )
+}
